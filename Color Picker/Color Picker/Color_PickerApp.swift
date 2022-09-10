80a02a4bf1c8d6fd6a7172fb0dc0e7f270677bcf
Elaidzha1940 <eli080293@gmail.com>
2022-09-10 21:20:08 +0300

    10.09

---
+++ Color Picker/Color Picker/Color_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_PickerApp.swift
+//  Color Picker
+//
+//  Created by Elaidzha Shchukin on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Color_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color Picker/Color Picker/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Color Picker
+//
+//  Created by Elaidzha Shchukin on 10.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var selectedColor: Color = .black
+    
+    var body: some View {
+        VStack {
+            Circle()
+                .foregroundColor(selectedColor)
+                .frame(width: 200, height: 200)
+                .padding()
+            
+            Form {
+                ColorPicker("Change Circle Color", selection: $selectedColor)
+                    .padding()
+            }
+        
+           
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
